052fd2700f0bb2f2db2d1a196620cc089be089b5
AlexMihax <mihaipasca2204@yahoo.com>
2018-12-04 22:16:50 +0200

    Adaugare setari

--- Minigames1/Form2.Designer.cs
+++ Minigames1/Form2.Designer.cs
@@ -29,107 +29,148 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Minesweeper));
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.difficultyPanel = new System.Windows.Forms.Panel();
-            this.nivel = new System.Windows.Forms.Label();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.backButton = new System.Windows.Forms.PictureBox();
-            this.difficultyPanel.SuspendLayout();
+            this.usor = new System.Windows.Forms.CheckBox();
+            this.avansat = new System.Windows.Forms.CheckBox();
+            this.mediu = new System.Windows.Forms.CheckBox();
+            this.nivel = new System.Windows.Forms.Label();
+            this.Color = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.backButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(7, 51);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(48, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Usor";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // difficultyPanel
-            // 
-            this.difficultyPanel.Controls.Add(this.nivel);
-            this.difficultyPanel.Controls.Add(this.checkBox3);
-            this.difficultyPanel.Controls.Add(this.checkBox2);
-            this.difficultyPanel.Controls.Add(this.checkBox1);
-            this.difficultyPanel.Location = new System.Drawing.Point(184, 137);
-            this.difficultyPanel.Name = "difficultyPanel";
-            this.difficultyPanel.Size = new System.Drawing.Size(203, 122);
-            this.difficultyPanel.TabIndex = 1;
-            // 
-            // nivel
-            // 
-            this.nivel.AutoSize = true;
-            this.nivel.BackColor = System.Drawing.Color.Transparent;
-            this.nivel.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold);
-            this.nivel.Location = new System.Drawing.Point(3, 14);
-            this.nivel.Name = "nivel";
-            this.nivel.Size = new System.Drawing.Size(169, 23);
-            this.nivel.TabIndex = 4;
-            this.nivel.Text = "Nivel de dificultate";
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(7, 74);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(55, 17);
-            this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "Mediu";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(7, 97);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(65, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "Avansat";
-            this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // backButton
             // 
             this.backButton.BackColor = System.Drawing.Color.Transparent;
             this.backButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.backButton.Image = ((System.Drawing.Image)(resources.GetObject("backButton.Image")));
-            this.backButton.Location = new System.Drawing.Point(12, 12);
+            this.backButton.Location = new System.Drawing.Point(9, 1);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(130, 71);
+            this.backButton.Size = new System.Drawing.Size(51, 45);
             this.backButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.backButton.TabIndex = 4;
             this.backButton.TabStop = false;
-            this.backButton.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.backButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // usor
+            // 
+            this.usor.AutoSize = true;
+            this.usor.BackColor = System.Drawing.Color.Transparent;
+            this.usor.ForeColor = System.Drawing.Color.MediumBlue;
+            this.usor.Location = new System.Drawing.Point(61, 115);
+            this.usor.Name = "usor";
+            this.usor.Size = new System.Drawing.Size(48, 17);
+            this.usor.TabIndex = 0;
+            this.usor.Text = "Usor";
+            this.usor.UseVisualStyleBackColor = false;
+            this.usor.CheckedChanged += new System.EventHandler(this.Usor_CheckedChanged);
+            // 
+            // avansat
+            // 
+            this.avansat.AutoSize = true;
+            this.avansat.BackColor = System.Drawing.Color.Transparent;
+            this.avansat.ForeColor = System.Drawing.Color.MediumBlue;
+            this.avansat.Location = new System.Drawing.Point(61, 161);
+            this.avansat.Name = "avansat";
+            this.avansat.Size = new System.Drawing.Size(65, 17);
+            this.avansat.TabIndex = 1;
+            this.avansat.Text = "Avansat";
+            this.avansat.UseVisualStyleBackColor = true;
+            // 
+            // mediu
+            // 
+            this.mediu.AutoSize = true;
+            this.mediu.BackColor = System.Drawing.Color.Transparent;
+            this.mediu.ForeColor = System.Drawing.Color.MediumBlue;
+            this.mediu.Location = new System.Drawing.Point(61, 138);
+            this.mediu.Name = "mediu";
+            this.mediu.Size = new System.Drawing.Size(55, 17);
+            this.mediu.TabIndex = 2;
+            this.mediu.Text = "Mediu";
+            this.mediu.UseVisualStyleBackColor = true;
+            // 
+            // nivel
+            // 
+            this.nivel.AutoSize = true;
+            this.nivel.BackColor = System.Drawing.Color.Transparent;
+            this.nivel.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold);
+            this.nivel.ForeColor = System.Drawing.Color.MediumBlue;
+            this.nivel.Location = new System.Drawing.Point(74, 80);
+            this.nivel.Name = "nivel";
+            this.nivel.Size = new System.Drawing.Size(169, 23);
+            this.nivel.TabIndex = 4;
+            this.nivel.Text = "Nivel de dificultate";
+            this.nivel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.nivel.Click += new System.EventHandler(this.Nivel_Click);
+            // 
+            // Color
+            // 
+            this.Color.AutoSize = true;
+            this.Color.BackColor = System.Drawing.Color.Transparent;
+            this.Color.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold);
+            this.Color.ForeColor = System.Drawing.Color.MediumBlue;
+            this.Color.Location = new System.Drawing.Point(78, 185);
+            this.Color.Name = "Color";
+            this.Color.Size = new System.Drawing.Size(148, 23);
+            this.Color.TabIndex = 5;
+            this.Color.Text = "Alegeti culoarea";
+            // 
+            // button1
+            // 
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.Location = new System.Drawing.Point(219, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 27);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(91, 226);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 8;
             // 
             // Minesweeper
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(658, 410);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Color);
+            this.Controls.Add(this.avansat);
+            this.Controls.Add(this.mediu);
+            this.Controls.Add(this.nivel);
             this.Controls.Add(this.backButton);
-            this.Controls.Add(this.difficultyPanel);
+            this.Controls.Add(this.usor);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Minesweeper";
             this.Text = "Minesweeper";
-            this.difficultyPanel.ResumeLayout(false);
-            this.difficultyPanel.PerformLayout();
+            this.Load += new System.EventHandler(this.Minesweeper_Load);
             ((System.ComponentModel.ISupportInitialize)(this.backButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Panel difficultyPanel;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.PictureBox backButton;
+        private System.Windows.Forms.CheckBox usor;
+        private System.Windows.Forms.CheckBox avansat;
+        private System.Windows.Forms.CheckBox mediu;
         private System.Windows.Forms.Label nivel;
+        private System.Windows.Forms.Label Color;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
--- Minigames1/Form2.cs
+++ Minigames1/Form2.cs
@@ -16,17 +16,40 @@ namespace Minigames1
         {
             InitializeComponent();
         }
-            private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void Usor_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Avansat_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Mediu_CheckedChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void PictureBox1_Click(object sender, EventArgs e)
+        private void BackButton_Click(object sender, EventArgs e)
         {
             this.Hide();
             var form1 = new MiniGames();
             form1.Closed += (s, args) => this.Close();
             form1.Show();
+        }
+
+        private void DifficultyPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Nivel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Minesweeper_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
